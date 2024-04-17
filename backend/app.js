76ad9de3eb4fae5e665fc4708737b0770c14e8e7
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-04-17 07:25:11 +0530

    Creating Routes>CourseController(for sending respose)>app.js(import routes(app.use()))

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,21 @@
 import express from "express";
+import { config } from "dotenv";
+
+
+
+config({
+    path:"./config/config.env",
+});
 
 const app = express();
+
+
+// Importing & Using Routes
+import course from "./routes/courseRoutes.js";
+import user from "./routes/userRoutes.js";
+
+app.use("/api/v1",course)
+app.use("/api/v1",user)
+
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,6 @@
-import { config } from "dotenv";
 import app from "./app.js";
 
-config({
-    path:"./config/config.env",
-});
+
 
 app.listen(process.env.PORT, ()=>{
     console.log(`server is working on port: ${process.env.PORT}`)
