0a27ee87186de82d47c490b418ca1dc43a46ac24
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-04-19 19:25:33 +0530

    user routes , usercontrol, api(Postman) , jwttoken , env , model(user.js)

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,29 @@
 import express from "express";
+import { register } from "../controllers/userController.js";
 
 
 const router = express.Router();
 
+// to register a new User
+router.route('/register').post(register);
+
+//login
+// logout
+//getmyprofile
+
+
+//change password
+//updateprofile
+// update profile picture
+
+
+
+//forgetpassword
+//resetpassword
+
+
+//add to playlist
+//remove from playlist
 
 
 
